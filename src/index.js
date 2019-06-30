269cd8d65f70df3b3e1b53b9cf3bec2255d9b7c3
Megan Fago <meganlaynefago@gmail.com>
2019-06-30 11:46:31 -0400

    added spinner and updated szn display

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner'
 
 
 class App extends Component {
@@ -30,7 +31,7 @@ class App extends Component {
            return <SeasonDisplay latitude={this.state.latitude}/>
         }
 
-        return <div>Loading...</div>;
+        return <Spinner/> ;
     }
 }
 
